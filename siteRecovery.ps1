20804080591bf9620e701bc4bba98d10892c2123
matthieugirard <matthieugirard@outlook.com>
2023-03-09 15:59:35 +0100

    first commit

---
+++ siteRecovery.ps1
@@ -0,0 +1,125 @@
+# Get details of the virtual machine
+# $vms = @("hastus-front-1", "hastus-front-2")
+$vm = Get-AzVM -ResourceGroupName "hastus" -Name "hastus-front-1"
+
+#Get the vault
+$vault = Get-AzRecoveryServicesVault -Name hastus-rv -ResourceGroupName hastus
+
+#Setting the vault context.
+Set-AzRecoveryServicesAsrVaultContext -Vault $vault
+
+#Create ASR fabric
+$TempASRJob = New-AzRecoveryServicesAsrFabric -Azure -Location 'West Europe'  -Name "hastus-westeurope-fabric"
+
+# Track Job status to check for completion
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        #If the job hasn't completed, sleep for 10 seconds before checking the job status again
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+#Check if the Job completed successfully. The updated job state of a successfully completed job should be "Succeeded"
+Write-Output $TempASRJob.State
+
+$PrimaryFabric = Get-AzRecoveryServicesAsrFabric -Name "hastus-westeurope-fabric"
+$RecoveryFabric = $PrimaryFabric
+
+#Create a Protection container in the primary Azure region (within the Primary fabric)
+$TempASRJob = New-AzRecoveryServicesAsrProtectionContainer -InputObject $PrimaryFabric -Name "hastus-front-1-we-source-container"
+
+#Track Job status to check for completion
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+Write-Output $TempASRJob.State
+
+$PrimaryProtContainer = Get-AzRecoveryServicesAsrProtectionContainer -Fabric $PrimaryFabric -Name "hastus-front-1-we-source-container"
+
+#Create a Protection container in the recovery Azure region (within the Recovery fabric)
+$TempASRJob = New-AzRecoveryServicesAsrProtectionContainer -InputObject $RecoveryFabric -Name "hastus-front-1-we-destination-container"
+
+#Track Job status to check for completion
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+#Check if the Job completed successfully. The updated job state of a successfully completed job should be "Succeeded"
+
+Write-Output $TempASRJob.State
+
+$RecoveryProtContainer = Get-AzRecoveryServicesAsrProtectionContainer -Fabric $RecoveryFabric -Name "hastus-front-1-we-destination-container"
+
+#Create replication policy
+$TempASRJob = New-AzRecoveryServicesAsrPolicy -AzureToAzure -Name "hastus-front-1-policy" -RecoveryPointRetentionInHours 24 -ApplicationConsistentSnapshotFrequencyInHours 4
+
+#Track Job status to check for completion
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+#Check if the Job completed successfully. The updated job state of a successfully completed job should be "Succeeded"
+Write-Output $TempASRJob.State
+
+$ReplicationPolicy = Get-AzRecoveryServicesAsrPolicy -Name "hastus-front-1-policy"
+
+#Create Protection container mapping between the Primary and Recovery Protection Containers with the Replication policy
+$TempASRJob = New-AzRecoveryServicesAsrProtectionContainerMapping -Name "hastus-front-1-mapping" -Policy $ReplicationPolicy -PrimaryProtectionContainer $PrimaryProtContainer -RecoveryProtectionContainer $RecoveryProtContainer
+
+#Track Job status to check for completion
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+#Check if the Job completed successfully. The updated job state of a successfully completed job should be "Succeeded"
+Write-Output $TempASRJob.State
+
+$hastusZoneProtectionMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $PrimaryProtContainer -Name "hastus-front-1-mapping"
+
+#Create Protection container mapping (for fail back) between the Recovery and Primary Protection Containers with the Replication policy
+$TempASRJob = New-AzRecoveryServicesAsrProtectionContainerMapping -Name "hastus-front-1-mapping-reverse" -Policy $ReplicationPolicy -PrimaryProtectionContainer $RecoveryProtContainer -RecoveryProtectionContainer $PrimaryProtContainer
+
+#Track Job status to check for completion
+while (($TempASRJob.State -eq "InProgress") -or ($TempASRJob.State -eq "NotStarted")){
+        sleep 10;
+        $TempASRJob = Get-AzRecoveryServicesAsrJob -Job $TempASRJob
+}
+
+#Check if the Job completed successfully. The updated job state of a successfully completed job should be "Succeeded"
+Write-Output $TempASRJob.State
+
+$hastusReverseZoneProtectionMapping = Get-AzRecoveryServicesAsrProtectionContainerMapping -ProtectionContainer $RecoveryProtContainer -Name "hastus-front-1-mapping"
+
+#Create Cache storage account for replication logs in the primary region
+# $hastusCacheStorageAccount = New-AzStorageAccount -Name "hastusreplicache" -ResourceGroupName "hastus" -Location 'West Europe' -SkuName Standard_LRS -Kind Storage
+
+$hastusCacheStorageAccount = Get-AzStorageAccount -ResourceGroupName "hastus" -Name "hastusreplicache"
+
+#Get the resource group that the virtual machine must be created in when failed over.
+$RecoveryRG = Get-AzResourceGroup -Name "hastus-dest" -Location "West Europe"
+
+#Specify replication properties for each disk of the VM that is to be replicated (create disk replication configuration)
+
+#OsDisk
+$OSdiskId = $vm.StorageProfile.OsDisk.ManagedDisk.Id
+# $RecoveryOSDiskAccountType = $vm.StorageProfile.OsDisk.ManagedDisk.StorageAccountType
+$RecoveryOSDiskAccountType = "StandardSSD_LRS"
+# $RecoveryReplicaDiskAccountType = $vm.StorageProfile.OsDisk.ManagedDisk.StorageAccountType
+$RecoveryReplicaDiskAccountType = "StandardSSD_RS"
+
+
+$OSDiskReplicationConfig = New-AzRecoveryServicesAsrAzureToAzureDiskReplicationConfig -ManagedDisk -LogStorageAccountId $hastusCacheStorageAccount.Id `
+         -DiskId $OSdiskId -RecoveryResourceGroupId  $RecoveryRG.ResourceId -RecoveryReplicaDiskAccountType  $RecoveryReplicaDiskAccountType `
+         -RecoveryTargetDiskAccountType $RecoveryOSDiskAccountType
+
+
+#Create a list of disk replication configuration objects for the disks of the virtual machine that are to be replicated.
+$diskconfigs = @()
+$diskconfigs += $OSDiskReplicationConfig
+
+#Start replication by creating replication protected item. Using a GUID for the name of the replication protected item to ensure uniqueness of name.
+$TempASRJob = New-AzRecoveryServicesAsrReplicationProtectedItem -AzureToAzure -AzureVmId $VM.Id -Name (New-Guid).Guid -ProtectionContainerMapping $hastusZoneProtectionMapping -AzureToAzureDiskReplicationConfiguration $diskconfigs -RecoveryResourceGroupId $RecoveryRG.ResourceId -RecoveryAvailabilitySetId "/subscriptions/68be309e-79c1-483f-886f-0351d776e20c/resourceGroups/HASTUS-DEST/providers/Microsoft.Compute/availabilitySets/hastus-dest-front" -RecoveryProximityPlacementGroupId "/subscriptions/68be309e-79c1-483f-886f-0351d776e20c/resourceGroups/hastus-dest/providers/Microsoft.Compute/proximityPlacementGroups/hastus-dest-ppg"
